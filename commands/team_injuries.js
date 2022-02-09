c65dde09af20fb93eed34214422a4860bda17978
KevinKossi <mpungakevin@hotmail.com>
2022-02-09 02:28:56 +0100

    message has been changed

--- commands/team_injuries.js
+++ commands/team_injuries.js
@@ -10,7 +10,8 @@ module.exports = {
         if (args.length === 0) return;
         let team = isTeam(teams, args[0].toLowerCase());
         if (team === false){
-            msg.channel.send("That team is not recognized. Please use the !team_names command to see a list of valid teams.");
+            msg.channel.send("That team is not recognized. Please use the !teams command to see a list of valid teams.");
+            
             return;
         }
         const team_api_name = teams[team];
