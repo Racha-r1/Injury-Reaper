db13cbad67669221a3c19e244763700656b5fb0c
Abdirahman <rizetracker@gmail.com>
2022-04-09 19:38:40 +0200

    team names fix

--- commands/standings.js
+++ commands/standings.js
@@ -3,6 +3,11 @@ const axios = require('axios');
 const team_json = require("../teams_fullname.json");
 require('dotenv').config();
 
+const getTeam = (team_name) => {
+  if (team_name.indexOf('-') === -1) return team_name;
+  return team_name.slice(0,team_name.indexOf('-')).trim();
+}
+
 module.exports = {
     name: 'standings',
     description: 'Get the standings of a specific conference',
@@ -11,7 +16,7 @@ module.exports = {
         const teams = new MessageEmbed()
           .setColor("#0099ff")
           .setTitle(data.conference)
-          .setDescription(`${data.standings.map(standing => `${standing.rank}. ${team_json[standing.team.toLowerCase()]} \`${standing.wins} - ${standing.losses}\``).join("\n")}`)
+          .setDescription(`${data.standings.map(standing => `${standing.rank}. ${team_json[getTeam(standing.team).toLowerCase()]} \`${standing.wins} - ${standing.losses}\``).join("\n")}`)
           .setFooter({text: '@Copyright Mbongo Bueno Corporations'})
         msg.channel.send({embeds: [teams]});
     }
